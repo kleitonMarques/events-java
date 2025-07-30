32708fec432577431bf1b5e4ba7170c3622d64c6
Kleiton Marques <kleitonm.marques@gmail.com>
2025-07-30 00:04:59 -0300

    Create repo and service subscription

---
+++ src/main/java/br/com/nlw/events/service/SubscriptionService.java
@@ -0,0 +1,34 @@
+package br.com.nlw.events.service;
+
+import br.com.nlw.events.model.Event;
+import br.com.nlw.events.model.Subscription;
+import br.com.nlw.events.model.User;
+import br.com.nlw.events.repo.EventRepo;
+import br.com.nlw.events.repo.SubscriptionRepo;
+import br.com.nlw.events.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SubscriptionService {
+
+    @Autowired
+    private EventRepo evtRepo;
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private SubscriptionRepo subRepo;
+
+    public Subscription createNewSubscription(String eventName, User user) {
+        Event evt = evtRepo.findByPrettyName(eventName);
+        user = userRepo.save(user);
+
+        Subscription subs = new Subscription();
+        subs.setEvent(evt);
+        subs.setSubscriber(user);
+
+        return subRepo.save(subs);
+    }
+}
